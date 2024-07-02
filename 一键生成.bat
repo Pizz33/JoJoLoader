3070b55c303200a0a3216194b75f51b2c409db29
hyyrent <1721786810@qq.com>
2024-07-02 09:32:52 +0800

    Update 一键生成.bat

--- 一键生成.bat
+++ 一键生成.bat
@@ -3,15 +3,11 @@ setlocal EnableDelayedExpansion
 
 python encrypt.py
 
-//cargo clean
-
 cargo build --release
 
-:: 设置源文件和目标目录
 set "sourceFile=target\release\project.exe"
 set "targetDir=output"
 
-:: 生成6位随机英文文件名
 set "chars=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 set "randomName="
 
@@ -20,21 +16,16 @@ for /l %%i in (1,1,6) do (
     for %%j in (!randIndex!) do set "randomName=!randomName!!chars:~%%j,1!"
 )
 
-:: 构建目标文件路径
 set "targetFile=%targetDir%\!randomName!.exe"
 
-:: 复制文件
 copy "%sourceFile%" "%targetFile%"
 
-:: 输出结果
 echo Copied and renamed to: "%targetFile%"
 
-:: 设置Python脚本和依赖文件的路径
 set "pythonScript=output\sigthief.py"
 set "restoreExe=output\360Restore.exe"
 set "outputExe=%targetFile%_sign.exe"
 
-:: 运行 Python 脚本
 python "%pythonScript%" -t "%targetFile%" -i "%restoreExe%" -o "%outputExe%"
 
 endlocal
